3df93a95a06263cbb4f2b90f5857880db9c5a1c4
Suzanne Dyson <suzanne.dyson2334@gmail.com>
2019-10-14 16:58:29 +0100

    first tests passing

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ManagerTest {
+    Manager manager;
+
+    @Before
+    public void before() {
+        manager = new Manager("Fred", "DA 34 65 78 T",
+                45675.34, "IT");
+    }
+
+    @Test
+    public void managerHasName() {
+        assertEquals("Fred", manager.getName());
+    }
+
+    @Test
+    public void managerHasNiNumber() {
+        assertEquals("DA 34 65 78 T", manager.getNiNumber());
+    }
+
+    @Test
+    public void managerHasSalary() {
+        assertEquals(45675.34, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void managerHsaDeptName() {
+        assertEquals("IT", manager.getDeptName());
+    }
+
+    @Test
+    public void managerReceivesSalaryRise() {
+        manager.raiseSalary(3000.00);
+        assertEquals(48675.34, manager.getSalary(), 0.01);
+    }
+}
